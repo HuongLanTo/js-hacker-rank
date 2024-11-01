c320f1b21c8ebb18436c57feac7de33a8f8767f2
HuongLanTo <tohuonglan1705@gmail.com>
2024-10-31 22:24:52 -0500

    Minimum Loss

---
+++ minimum-loss.js
@@ -0,0 +1,18 @@
+function minimumLoss(price) {
+    // Write your code here
+    const priceWithIndices = price.map((value, index) => [value, index]);
+    
+    priceWithIndices.sort((a, b) => a[0] - b[0]);
+    
+    let res = Number.MAX_VALUE;
+    for (let i = 1; i < priceWithIndices.length; i++) {
+        const [curValue, curIndex] = priceWithIndices[i];
+        const [preValue, preIndex] = priceWithIndices[i - 1];
+        
+        if (curIndex < preIndex) {
+            res = Math.min(res, curValue - preValue);
+        }
+    }
+    
+    return res;
+}
